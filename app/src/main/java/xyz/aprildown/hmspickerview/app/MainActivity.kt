f2360c5f7ecb108cc81421cf25d74d9a9033b931
Reed <deweyreed@protonmail.com>
2019-02-28 20:48:11 +0800

    Add dialog example and night mode example

--- app/src/main/java/xyz/aprildown/hmspickerview/app/MainActivity.kt
+++ app/src/main/java/xyz/aprildown/hmspickerview/app/MainActivity.kt
@@ -1,9 +1,12 @@
 package xyz.aprildown.hmspickerview.app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
+import xyz.aprildown.hmspickerview.HmsPickerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnNight.setOnClickListener {
+            val isNight =
+                AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+            if (isNight) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            recreate()
+        }
+
         btnDialog.setOnClickListener {
-            AlertDialog.Builder(this)
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("HmsPickerView")
                 .setView(R.layout.layout_picker)
+                .setPositiveButton(android.R.string.ok, null)
+                .setNegativeButton(android.R.string.cancel, null)
                 .show()
+
+            val hmsPickerView = dialog.findViewById<HmsPickerView>(R.id.hmsPickerView)!!
+            dialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(android.R.string.ok)) { _, _ ->
+                Toast.makeText(
+                    this, "%2dh %2dm %2ds".format(
+                        hmsPickerView.getHours(),
+                        hmsPickerView.getMinutes(),
+                        hmsPickerView.getSeconds()
+                    ),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
